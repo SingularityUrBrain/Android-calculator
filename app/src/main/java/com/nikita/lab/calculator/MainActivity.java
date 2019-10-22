a75b389d832514303cbd9d10620cfbe2446c3f41
Nikita <nikita_kamzeev@mail.ru>
2019-10-22 19:38:25 +0300

    Add base fragment

--- app/src/main/java/com/nikita/lab/calculator/MainActivity.java
+++ app/src/main/java/com/nikita/lab/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.nikita.lab.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,20 +12,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.commons.jexl2.Expression;
+import org.apache.commons.jexl2.JexlArithmetic;
 import org.apache.commons.jexl2.JexlContext;
 import org.apache.commons.jexl2.JexlEngine;
 import org.apache.commons.jexl2.MapContext;
 
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements BaseFragment.OnFragmentInteractionListener {
 
     TextView textView;
     ScrollView mainScrollView;
     EditText editText;
 
     String PADDING = "\n\n\n\n\n";
-    StringBuilder buff = new StringBuilder(PADDING);
-    Double result = 0.;
+    //StringBuilder buff = new StringBuilder(PADDING);
+    StringBuilder result = new StringBuilder("0");
+    double answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,31 +49,82 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-//
-//    public void Calc(View view) {
-//        JexlEngine jexl = new JexlEngine();
-//        String jexlExp = "3+4*(3+4)";
-//        Expression e = jexl.createExpression( jexlExp );
-//        JexlContext jctx = new MapContext();
-//        Object o = e.evaluate(jctx);
-//        Toast.makeText(this, o.toString(), Toast.LENGTH_LONG).show();
-//    }
+
+    @Override
+    public void onFragmentInteraction(String link) {
+        BaseFragment fragment = (BaseFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.baseFragment);
+//        if (fragment != null && fragment.isInLayout()) {
+//            fragment.onAction(link);
+//        }
+    }
+
     public void OnOpClick(View view){
         Button button = (Button)view;
         String op = button.getText().toString();
-        if (op.equals("C")){
-            buff.setLength(0);
-            buff.append(PADDING);
-            editText.setText("0");
-            result = 0.;
-        }
-        else {
-            buff.append(editText.getText());
-            buff.append('\n');
-            editText.setText(op);
+        switch (op) {
+            case "C":
+                textView.setText("");
+                textView.append(PADDING);
+                editText.setText("0");
+                result.setLength(0);
+                result.append("0");
+                break;
+            case "⌫":
+                Editable txt = editText.getText();
+                editText.setText(txt.delete(txt.length() - 1, txt.length()));
+                result.setLength(result.length() - 1);
+                return;
+            case "=":
+                JexlEngine jexl = new JexlEngine();
+                String jexlExp = result.toString();
+                Expression e = jexl.createExpression(jexlExp);
+                Object o = e.evaluate(null);
+                StringBuilder ans_str = new StringBuilder(o.toString());
+                result = new StringBuilder(ans_str);
+                textView.append(editText.getText());
+                textView.append("\n");
+                editText.setText(ans_str.insert(0, "= "));
+                break;
+            default:
+                String op_eval;
+                switch (op) {
+                    case "−":
+                        op_eval = "-";
+                        break;
+                    case "×":
+                        op_eval = "*";
+                        break;
+                    case "÷":
+                        op_eval = "/";
+                        break;
+                    default:
+                        op_eval = op;
+                        break;
+                }
+                // TODO: change sign if sign in editText now else add sign
+                result.append(op_eval);
+                textView.append(editText.getText());
+                textView.append("\n");
+                editText.setText(op);
+                editText.append(" ");
+                break;
         }
 
-        textView.setText(buff);
+        //textView.setText(buff);
         mainScrollView.fullScroll(ScrollView.FOCUS_DOWN);
+    }
+    public void OnNumberClick(View view){
+        Button button = (Button)view;
+        String num = button.getText().toString();
+        if (result.charAt(0) == '0'){
+            textView.append(PADDING);
+            result.setLength(0);
+            editText.setText(num);
+        }
+        else{
+            editText.append(num);
+        }
+        result.append(num);
     }
 }
